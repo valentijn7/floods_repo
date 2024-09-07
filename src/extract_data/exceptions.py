e6b76e26108f02236cfd48fe8faa1f601dbd914c
valentijn7 <v.w.oldenburg@student.rug.nl>
2024-09-07 20:18:06 +0200

    Initial commit, w/ some initial prep for handling all of the data

---
+++ src/extract_data/exceptions.py
@@ -0,0 +1,5 @@
+class MissingKeyError(Exception):
+    def __init__(self, key, file):
+        self.key = key
+        self.file = file
+        super().__init__(f"Missing key '{key}' in file '{file}'")
