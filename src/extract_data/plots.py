2c00ca4e7f7dec25c7ca6e63bc7bd45ec1e35ad3
valentijn7 <v.w.oldenburg@student.rug.nl>
2024-10-07 12:14:23 +0200

    Plotjes werken nu

--- src/extract_data/plots.py
+++ src/extract_data/plots.py
@@ -30,10 +30,10 @@ def set_TeX_style() -> None:
     plt.rcParams.update({
         "text.usetex": True,
         "font.family": "Computer Modern Serif",
-        "axes.labelsize": 8,     # fontsize for x and y labels
-        "xtick.labelsize": 8,    # fontsize of the tick labels
-        "ytick.labelsize": 8,    # fontsize of the tick labels
-        "legend.fontsize": 8,    # fontsize of the legen
+        "axes.labelsize": 10,     # fontsize for x and y labels
+        "xtick.labelsize": 10,    # fontsize of the tick labels
+        "ytick.labelsize": 10,    # fontsize of the tick labels
+        "legend.fontsize": 10,    # fontsize of the legen
     })
 
 
@@ -56,6 +56,193 @@ def set_plot_style(
         set_TeX_style()
     sns.set_theme(style = 'ticks')
     sns.set_context(context) if context else None
+
+
+def make_subset_for_gauge_and_issue_time(
+        df: pd.DataFrame, gauge: str, issue_date: datetime.datetime) -> pd.DataFrame:
+    """
+    Create a subset of a DataFrame for a specific gauge and issue time
+
+    :param df: DataFrame with forecasted values
+    :param gauge: ID of the gauge
+    :param issue_time: issue time of the forecast
+    :return: subset of the DataFrame
+    """
+    if not all(df['gauge_ID'].apply(lambda x: isinstance(x, str))):
+        df['gauge_ID'] = df['gauge_ID'].astype(str)
+    if not all(df['issue_date'].apply(lambda x: isinstance(x, datetime.date))):
+        df['issue_date'] = pd.to_datetime(df['issue_date']).dt.date
+    assert all(df['gauge_ID'].apply(lambda x: isinstance(x, str))), \
+        "gauge_ID column contains non-string values, subseting hindered"
+    assert all(df['issue_date'].apply(lambda x: isinstance(x, datetime.date))), \
+        "issue_date column contains non-date values, subsetting hindered"
+
+    return df[(df['gauge_ID'] == gauge) & (df['issue_date'] == issue_date.date())]  
+
+
+def create_dates_series(start_date : datetime.datetime, delta: int) -> pd.Series:
+    """
+    Create a series of dates starting from a given date and going on for a given number of days
+
+    :param start_date: starting date
+    :param delta: number of days
+    :return: Series of dates
+    """
+    return pd.Series(
+        # minus one here because, strangely enough, the first forecasted
+        # date is one day in the past compared to the issue date...
+        [start_date.date() + datetime.timedelta(days = idx - 1) for idx in range(0, delta + 1)]
+    )
+
+
+def set_custom_date_ticks(ax: plt.Axes, dates: pd.Series) -> None:
+    """
+    Set custom date ticks on the x-axis of a plot, where only
+    the first and final date are displayed, while keeping the ticks
+
+    :param ax: Axes object
+    :param dates: Series of dates (datetime.date objects)
+    """
+    ax.set_xticks(dates)
+    x_labels = [
+        dates.iloc[0].strftime('%Y-%m-%d')
+    ] + [''] * (len(dates) - 2) + [
+        dates.iloc[-1].strftime('%Y-%m-%d')
+    ]
+    ax.set_xticklabels(x_labels)  
+
+
+def plot_gauge_forecast_for_issue_time(
+        df : pd.DataFrame, gauge: str, issue_date : datetime.datetime, country : str = None) -> None:
+    """
+    Plots with a graph the forecasted values for a specific gauge and issue time
+
+    :param df: DataFrame with forecasted values
+    :param gauge: ID of the gauge
+    :param issue_time: issue time of the forecast
+    :param country: name of the country
+    """
+    set_plot_style()
+
+    df_subset = make_subset_for_gauge_and_issue_time(df, gauge, issue_date)
+    if df_subset.empty:
+        print(f"No forecasted values for gauge {gauge} at {issue_date.date()}")
+        return
+
+    ax = sns.lineplot(
+        x = 'fc_date',
+        y = 'fc_value',
+        data = df_subset,
+        color = '#DB0A13'
+    )
+
+    # only display first and final forecast date on date-axis while keeping ticks
+    set_custom_date_ticks(
+        ax,
+        create_dates_series(issue_date, 7)
+    )
+
+    plt.title(f'Forecasts for gauge {gauge} in {country}, w/ 1st issue date: {issue_date.date()}')
+    plt.xlabel('date')
+    plt.ylabel('[m³/s]')
+    plt.show()
+
+
+def plot_week_of_gauge_forecast_for_issue_time(
+        df : pd.DataFrame, gauge: str, issue_date : datetime.datetime, country : str = None) -> None:
+    """
+    Plots the forecasted values for a specific gauge over a week of
+    issue times, giving seven graphs in total, each of (7 + 1 =) 8 days length 
+
+    :param df: DataFrame with forecasted values
+    :param gauge: ID of the gauge
+    :param issue_time: first issue time
+    :param country: Name of the country
+    """
+    set_plot_style()
+    plt.figure(figsize = (10, 6))
+    custom_palette = get_custom_palette(7)
+    
+    for idx in range(7): # loop for seven days, aka a week
+        df_subset = make_subset_for_gauge_and_issue_time(
+            df, gauge, issue_date + datetime.timedelta(days = idx)
+        )
+        if df_subset.empty:
+            print(f"No forecasted values for gauge {gauge} at {issue_date.date()}")
+            return
+
+        sns.lineplot(
+            x = 'fc_date',
+            y = 'fc_value',
+            data = df_subset,
+            color = custom_palette[idx]
+        )
+
+    set_custom_date_ticks(
+        plt.gca(),
+        create_dates_series(issue_date, 7 + 7) # a week plus lead time
+    )
+
+    plt.title(f'Forecasts for gauge {gauge} in {country}, w/ 1st issue date: {issue_date.date()}')
+    plt.xlabel('date')
+    plt.ylabel('[m³/s]')
+    plt.show()
+
+
+def plot_x_days_of_gauge_forecast_for_issue_time(
+        df : pd.DataFrame,
+        gauge: str,
+        issue_date : datetime.datetime,
+        days: int,
+        country : str = None,
+        TeX: bool = False,
+        export: bool = False) -> None:
+    """
+    Plots the forecasted values for a specific gauge over a month of
+    issue times, giving 30 graphs in total, each of (7 + 1 =) 8 days length 
+
+    :param df: DataFrame with forecasted values
+    :param gauge: ID of the gauge
+    :param issue_time: first issue time
+    :param country: Name of the country
+    """
+    set_plot_style()
+    set_TeX_style() if TeX else None
+    plt.figure(figsize = (10, 6))
+    custom_palette = get_custom_palette(days)
+    
+    for idx in range(days):
+        df_subset = make_subset_for_gauge_and_issue_time(
+            df, gauge, issue_date + datetime.timedelta(days = idx)
+        )
+        if df_subset.empty:
+            print(f"No forecasted values for gauge {gauge} at {issue_date.date()}")
+            return
+
+        sns.lineplot(
+            x = 'fc_date',
+            y = 'fc_value',
+            data = df_subset,
+            color = custom_palette[idx]
+        )
+
+    set_custom_date_ticks(
+        plt.gca(),
+        create_dates_series(issue_date, days + 7) # a week plus lead time
+    )
+
+    plt.title(f'Forecasts for gauge {gauge} in {country}, w/ 1st issue date: {issue_date.date()}')
+    plt.xlabel('date')
+    plt.ylabel('[m³/s]')
+    plt.draw()
+
+    if export:
+        plt.savefig(f"../../plots/graph_{days}_fcs_of_gauge_{gauge}_at_issue_date_{str(issue_date.date())}.pdf",
+                    format = 'pdf',
+                    bbox_inches = 'tight',
+                    pad_inches = 0.015)
+        
+    plt.show()
 
 
 def plot_danger_levels_hist(df: pd.DataFrame, country: str = None, bins: int = 10) -> None:
